d38b0912deb27cee679872dabde4df9b1cff4439
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-18 16:07:40 +0530

    Execute the db-script automatically when deploying

--- src/main/java/lk/ijse/dep8/tasks/DBInitializer.java
+++ src/main/java/lk/ijse/dep8/tasks/DBInitializer.java
@@ -5,10 +5,15 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,16 +24,33 @@ public class DBInitializer implements ServletContextListener {
 
     @Resource(name = "java:comp/env/jdbc/pool")
     private DataSource pool;
+
+    final String[] tableList = {"user", "task_list", "task", "sub_task"};
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        try(Connection connection = pool.getConnection()){
+        final String[] tables = {"task", "task_list", "user", "sub_task"};
+        final List<String> tableList = new ArrayList<>();
+
+        try (Connection connection = pool.getConnection()) {
+
             Statement stm = connection.createStatement();
             ResultSet rst = stm.executeQuery("SHOW TABLES");
 
-            while (rst.next()){
-                System.out.println(rst.getString(1));
+            while (rst.next()) {
+                tableList.add(rst.getString(1));
             }
-        }catch (SQLException e){
+
+            Arrays.sort(tables);
+            if (!Arrays.equals(tables, tableList.toArray())) {
+
+                InputStream is = this.getClass().getResourceAsStream("/db-script.sql");
+                byte[] bytes = new byte[is.available()];
+                is.read(bytes);
+                String sqlScript = new String(bytes);
+
+                stm.execute(sqlScript);
+            }
+        }catch (SQLException | IOException e) {
             logger.log(Level.SEVERE, e.getMessage(), e);
         }
     }
