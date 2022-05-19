86d58757c4fd1adede3d7c6eabc4b77ea2f45d4c
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-19 13:12:49 +0530

    Implement Server side validation logic for User details

--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.dep8.tasks.api;
 
 import lk.ijse.dep8.tasks.listener.DBInitializer;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
 import javax.annotation.Resource;
 import javax.servlet.*;
@@ -25,6 +26,20 @@ public class UserServlet extends HttpServlet {
             return;
         }
 
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Part picture = request.getPart("picture");
+
+        if(name == null || !name.matches("[A-Za-z ]+")){
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid name or name is empty");
+        }else if(email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid email or email is empty");
+        }else if(password == null || password.trim().isEmpty()){
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Email is empty");
+        }else if(picture != null && !picture.getContentType().startsWith("image")){
+            throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid picture");
+        }
 
     }
 }
