5adad3219d418efcd95f0611941b3f9a9511f407
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-19 16:08:14 +0530

    Implement register user logic - 2

--- src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/UserServlet.java
@@ -19,6 +19,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @MultipartConfig(location = "/tmp", maxFileSize = 10 * 1024 * 1024)
@@ -33,13 +34,13 @@ public class UserServlet extends HttpServlet2 {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request.getContentType() == null || !request.getContentType().startsWith("multipart/form-data")){
+        if (request.getContentType() == null || !request.getContentType().startsWith("multipart/form-data")) {
             response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE);
             return;
         }
 
 
-        if(request.getPathInfo() != null && !request.getPathInfo().equals("/")){
+        if (request.getPathInfo() != null && !request.getPathInfo().equals("/")) {
             throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "Invalid url");
         }
 
@@ -48,47 +49,64 @@ public class UserServlet extends HttpServlet2 {
         String password = request.getParameter("password");
         Part picture = request.getPart("picture");
 
-        if(name == null || !name.matches("[A-Za-z ]+")){
+        if (name == null || !name.matches("[A-Za-z ]+")) {
             throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid name or name is empty");
-        }else if(email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
+        } else if (email == null || !email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")) {
             throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid email or email is empty");
-        }else if(password == null || password.trim().isEmpty()){
+        } else if (password == null || password.trim().isEmpty()) {
             throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Email is empty");
-        }else if(picture != null && !picture.getContentType().startsWith("image")){
+        } else if (picture != null && !picture.getContentType().startsWith("image")) {
             throw new ResponseStatusException(HttpServletResponse.SC_BAD_REQUEST, "Invalid picture");
         }
 
-        String application = getServletContext().getRealPath("/");
-        Path path = Paths.get(application, "uploads");
-        if(Files.notExists(path)){
-            Files.createDirectory(path);
-        }
+        Connection connection = null;
+        try  {
 
-        try(Connection connection = pool.getConnection()){
-
+            connection = pool.getConnection();
             connection.setAutoCommit(false);
 
-            PreparedStatement stm = connection.prepareStatement("INSERT INTO user (id, emali, password, full_name) VALUES (UUID(), ?, ?, ?)");
-            stm.setString(1, email);
-            stm.setString(2, password);
-            stm.setString(3, name);
 
-            if(stm.executeUpdate() != 1){
+            String id = UUID.randomUUID().toString();
+            PreparedStatement stm = connection.prepareStatement("INSERT INTO user (id, email, password, full_name, profile_pic) VALUES (?,?, ?, ?, ?)");
+            stm.setString(1, id);
+            stm.setString(2, email);
+            stm.setString(3, password);
+            stm.setString(4, name);
+
+            String pictureUrl = request.getScheme() + "://" + request.getServerName() + ":"
+                    + request.getServerPort() + request.getContextPath();
+            pictureUrl += "/uploads/" + id ;
+
+            stm.setString(5, pictureUrl);
+
+            if (stm.executeUpdate() != 1) {
                 throw new SQLException("Failed to Register User");
             }
+            String appLocation = getServletContext().getRealPath("/");
+            Path path = Paths.get(appLocation, "uploads");
+            if (Files.notExists(path)) {
+                Files.createDirectory(path);
+            }
 
+            String picturePath = path.resolve(id).toAbsolutePath().toString();
+            picture.write(picturePath);
 
-            stm = connection.prepareStatement("SELECT id FROM user WHERE email = ?");
-            stm.setString(1, email);
-            ResultSet rst = stm.executeQuery();
-            rst.next();
-            String uuid = rst.getString("id");
+            if (Files.notExists(Paths.get(picturePath))){
+                throw new ResponseStatusException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to save the picture");
+            }
 
-            Path imagePath = path.resolve(uuid);
-            System.out.println(imagePath);
+            connection.commit();
 
-        }catch (SQLException e){
-            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new ResponseStatusException(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Failed to register the user");
+        } finally {
+            try {
+                connection.rollback();
+                connection.setAutoCommit(true);
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
