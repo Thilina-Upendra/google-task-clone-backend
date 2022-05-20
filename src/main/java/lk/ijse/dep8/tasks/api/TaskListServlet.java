f11a23ae221d63948c1439df7f377b8c8d2d69f6
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-20 11:04:28 +0530

    Add TaskListServlet to the project

---
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep8.tasks.api;
+
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "TaskListsServlet", value = "/v1/*")
+public class TaskListServlet extends HttpServlet {
+
+}
