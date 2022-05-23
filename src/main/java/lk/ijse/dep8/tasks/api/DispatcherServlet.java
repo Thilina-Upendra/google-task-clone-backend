5b21bfeb9b9c04183e704e8a88c8ab7119e2cd80
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-23 13:43:12 +0530

    Finalize dispatcher servlet

--- src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
@@ -1,12 +1,17 @@
 package lk.ijse.dep8.tasks.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
 import lk.ijse.dep8.tasks.security.SecurityContextHolder;
+import lk.ijse.dep8.tasks.util.HttpResponseErrorMessage;
 import lk.ijse.dep8.tasks.util.HttpServlet2;
+import lk.ijse.dep8.tasks.util.ResponseStatusException;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -29,10 +34,12 @@ public class DispatcherServlet extends HttpServlet2 {
             Matcher matcher = Pattern.compile(pattern).matcher(req.getPathInfo());
             if (matcher.find()){
                 String userId = matcher.group(1);
-                if (!userId.equals(SecurityContextHolder.getPrincipal().getId())){
+                if (!userId.equals(SecurityContextHolder.getPrincipal().getId())) {
+                    res.setContentType("application/json");
                     res.setStatus(403);
-                    return;
-                }
+                    Jsonb jsonb = JsonbBuilder.create();
+                    jsonb.toJson(new HttpResponseErrorMessage(new Date().getTime(), 403, null,
+                            "Permission denied", req.getRequestURI()), res.getWriter());
             }
             if(req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")){
                 /*UserServlet*/
@@ -48,6 +55,12 @@ public class DispatcherServlet extends HttpServlet2 {
                 /*/v1/users/{{user_id}}/lists/{{list_id}}/*/
 
                 getServletContext().getNamedDispatcher("TaskListServlet").forward(req, res);
+            }else{
+                res.setContentType("application/json");
+                res.setStatus(404);
+                Jsonb jsonb = JsonbBuilder.create();
+                jsonb.toJson(new HttpResponseErrorMessage(new Date().getTime(), 404, null,
+                        "Invalid location", req.getRequestURI()), res.getWriter());
             }
         }
     }
